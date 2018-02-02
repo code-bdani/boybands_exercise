01618557710c1bc8203ad0235e906262b58c475b
Brittany D. Smith <bdaniphotography@gmail.com>
2018-02-02 15:34:11 -0600

    Completed exercise and added some style

--- boybands.js
+++ boybands.js
@@ -2,68 +2,23 @@ let bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One
 let vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
 // Get a reference to the appropriate DOM element for bands
-const bandElement = document.getElementById(???);
+const bandElement = document.getElementById("boy-bands");
 
 // Get a reference to the appropriate DOM element for vegetables
-const veggieElement = ???;
+const veggieElement = document.getElementById("vegetables");
 
 // Execute a for loop that will iterate over the arrays
-for (let loopTracker = 0; loopTracker < ???; loopTracker += 1) {
+for (let loopTracker = 0; loopTracker < bands.length; loopTracker += 1) {
 
-  // Get a reference to the current item in the bands array
-  const currentBand = ???;
+// Get a reference to the current item in the bands array
+  const currentBand = bands[loopTracker];
 
-  // Update the innerHTML value of the DOM element for bands
+// Update the innerHTML value of the DOM element for bands
+  bandElement.innerHTML += `<li>${currentBand}</li>`;
 
-  // Get a reference to the current item in the vegetables array
-  const currentVeggie = ???;
+// Get a reference to the current item in the vegetables array
+const currentVeggie = vegetables[loopTracker];
 
-  // Update the innerHTML value of the DOM element for vegetables
+// Update the innerHTML value of the DOM element for vegetables
+  veggieElement.innerHTML += `<li>${currentVeggie}</li>`
 }
-
-
-
-
-
-
-
-
-
-
-// let bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
-// let vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
-
-// // Get a reference to the appropriate DOM element for bands
-// const bandElement = document.getElementById("boybands");
-
-// // Get a reference to the appropriate DOM element for vegetables
-// const veggieElement = document.getElementById("vegetables");
-
-// // Execute a for loop that will iterate over the arrays
-// for (let loopTracker = 0; loopTracker < bands.length; loopTracker += 1) {
-//   ??? += `<p> ${bands[loopTracker]}</p>`;
-//   // Get a reference to the current item in the bands array
-//   const currentBand = ???;
-
-//   // Update the innerHTML value of the DOM element for bands
-//   document.getElementById("boybands").innerHTML = currentBand;
-
-//   // Get a reference to the current item in the vegetables array
-//   const currentVeggie = ???;
-
-//   // Update the innerHTML value of the DOM element for vegetables
-//   document.getElementById("vegetables").innerHTML = currentVeggie;
-// }
-
-// // Execute a for loop that will iterate over the arrays
-// for (let loopTracker = 0; loopTracker < bands.length; loopTracker += 1) {
-//     for (let loopTracker = 0; loopTracker < vegetables.length; loopTracker += 1) {
-//         ;
-//     }
-//   // Get a reference to the current item in the bands array
-//   const currentBand = ???;
-
-//   // Get a reference to the current item in the vegetables array
-//   const currentVeggie = ???;
-
-// }
